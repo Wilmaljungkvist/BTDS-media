6c2c5ea1f5eea490659808acd18ac4a095c2ce74
Wilma <wl222ek@student.lnu.se>
2024-05-06 18:17:25 +0200

    Add base to sessionModel

--- src/models/sessionModel.js
+++ src/models/sessionModel.js
@@ -1,31 +1,11 @@
 import mongoose from 'mongoose'
 import { BASE_SCHEMA } from './baseSchema.js'
 
-const { isEmail } = validator;
-
 const sessionSchema = new mongoose.Schema({
-  firstName: {
+  data: {
     type: String,
-    required: [true, 'A name is required'],
+    required: true,
     trim: true
-  },
-  email: {
-    type: String,
-    required: [false, 'Email address is required.'],
-    trim: true,
-    validate: [isEmail, 'Please provide a valid email address.']
-  },
-  contactType: {
-    type: String,
-    required: [true, 'Contact type is required.']
-  },
-  message: {
-    type: String,
-    required: [true, 'Message is required.']
-  },
-  recipientEmail: {
-    type: String,
-    required: [true, 'Recipient email address is required.']
   }
 })
 
